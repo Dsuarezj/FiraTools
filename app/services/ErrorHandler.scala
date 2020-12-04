fffb43c40eb64d7d264a53858c3e8d40835df951
Denisse Suarez <dsuarez@thoughtworks.com>
2020-12-04 19:57:29 +0100

    Improve error message

--- app/services/ErrorHandler.scala
+++ app/services/ErrorHandler.scala
@@ -1,20 +1,31 @@
+import java.io.{PrintWriter, StringWriter}
+
 import play.api.http.HttpErrorHandler
 import play.api.mvc._
 import play.api.mvc.Results._
+
 import scala.concurrent._
+import scala.util.Try
 
 class ErrorHandler extends HttpErrorHandler {
 
   def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
     Future.successful({
-      print("A client error occurred: " + message)
+      print("A client error occurred: %s | Request uri: %s | Method: %s".format(message, request.uri, request.method))
       Status(statusCode)("Lo sentimos! un error... reportalo! 🔥")
     })
   }
 
   def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
     Future.successful({
-      print("A client error occurred: " + request.uri + request.method)
+      val sw = new StringWriter
+      Try{
+        exception.printStackTrace(new PrintWriter(sw))
+      }
+      print(
+        "A server error occurred: %s | Message: %s | Request uri: %s | Method: %s"
+        .format(sw.toString, exception.toString, request.uri, request.method)
+      )
       InternalServerError("Lo sentimos! un error ... reportalo! 🔥")
     })
   }
